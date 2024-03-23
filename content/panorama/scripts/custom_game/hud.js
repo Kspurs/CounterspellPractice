e53a2201b9e9550ebb3d086da46d72c0cc22a2f2
liangjiachen <1074847834@qq.com>
2024-03-23 12:52:09 +0800

    developing

--- content/panorama/scripts/custom_game/hud.js
+++ content/panorama/scripts/custom_game/hud.js
@@ -1,3 +1,8 @@
+$.Msg("hello")
+const available_abilities=[{heroname:'lion',abilityname:"lion_impale"},{heroname:'doom_bringer',abilityname:"doom_bringer_doom"}]
+available_abilities.forEach((value)=>{
+    $.Msg(value.abilityname)
+})
 function OnUseImpale(){
     $.Msg("use spike")
     GameEvents.SendCustomGameEventToServer("OnPlayerChosenAbility",{abilityname:"lion_impale",heroname:"lion"})
